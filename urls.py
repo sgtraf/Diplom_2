322c585877378570625204ba4fa7ec44459d8343
G Sh <conan@yandex.ru>
2025-05-03 20:42:59 +0300

    Добавил класс ссылок.

---
+++ urls.py
@@ -0,0 +1,11 @@
+class Url:
+    MAIN_URL = 'https://stellarburgers.nomoreparties.site'
+    CREATE_AND_REGISTRATION_USER_URL = '/api/auth/register' #POST
+    LOGIN_USER_URL = '/api/auth/login' #POST
+    LOGOUT_USER_URL = '/api/auth/logout' #POST
+    GET_USER_INFO_URL = '/api/auth/user' #GET
+    UPDATE_USER_INFO_URL = '/api/auth/user' #PATCH
+    DELETE_USER_URL = '/api/auth/user'  # DELETE
+    CREATE_ORDERS_URL = '/api/orders' #PUT
+    GET_ORDER_URL = '/api/orders' #GET
+
