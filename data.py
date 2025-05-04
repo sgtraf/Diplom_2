cd02eed755cbc97635285bce0cb0bcd27d9f13f2
G Sh <conan@yandex.ru>
2025-05-04 22:00:09 +0300

    Закончил тесты для логина пользователя.

--- data.py
+++ data.py
@@ -2,6 +2,6 @@ class MessageData:
 
     MESSEGE_TEXT_403 = "User already exists"
     MESSEGE_TEXT_403_WITHOUT_ONE = "Email, password and name are required fields"
+    MESSEGE_INC_LOGIN_401 = "email or password are incorrect"
 
-    MESSEGE_INC_LOGIN = 'Недостаточно данных для входа'
     MESSEGE_NOT_FIND_LOGIN = 'Учетная запись не найдена'
--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -9,13 +9,9 @@ class TestUserLogin:
                         'проверяем код и тело ответа')
     def test_user_login(self,generate_registration_data):
         with allure.step("Создаем пользователя"):
-            #print(generate_registration_data)
             LoginMethods.register_new_user_and_return_login_password(generate_registration_data)
-            #print(body.json())
         with allure.step("Логин под пользователем"):
-            #print(generate_registration_data)
             body = LoginMethods.login_in_system(generate_registration_data['email'], generate_registration_data['password'])
-            #print(body.json())
         with (allure.step("Проверяем, что код ответа и тело соответствует документации")):
             assert body.status_code == 200
             assert body.json()["accessToken"] != 0
@@ -23,3 +19,12 @@ class TestUserLogin:
             assert body.json()["success"] == True
             assert body.json()["user"]["email"] == generate_registration_data['email']
             assert body.json()["user"]["name"] == generate_registration_data['name']
+
+    def test_user_login_with_wrong_data(self, generate_registration_data_without_delete_method):
+        with allure.step("Логин под несуществующим пользователем"):
+            body = LoginMethods.login_in_system(generate_registration_data_without_delete_method['email'],
+                                                generate_registration_data_without_delete_method['password'])
+        with (allure.step("Проверяем, что код ответа и тело соответствует документации")):
+            assert body.status_code == 401
+            assert body.json()["success"] == False
+            assert body.json()["message"] == MessageData.MESSEGE_INC_LOGIN_401
