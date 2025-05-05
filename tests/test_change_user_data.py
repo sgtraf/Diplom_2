4c6c62b052d5fbe5bfcad5a54c15aaeff09a6f53
G Sh <conan@yandex.ru>
2025-05-05 16:31:48 +0300

    Закончил тесты на изменение данных пользователя.

--- tests/test_change_user_data.py
+++ tests/test_change_user_data.py
@@ -16,7 +16,7 @@ class TestChangeUserData:
     #В этом методе есть баг, повторно использовать email, который уже был когда-то использован при изменении
     # данных пользователя невозможно, даже если пользователь был удален. Необходимо каждый раз использовать
     # уникальные почтовые адреса
-    def test_change_user_data(self,generate_registration_data_for_change_user_data, user_data_field):
+    def test_change_user_data_with_authorization(self,generate_registration_data_for_change_user_data, user_data_field):
 
         with allure.step("Логин под пользователем"):
             body = LoginMethods.login_in_system(generate_registration_data_for_change_user_data['email'],
@@ -36,13 +36,24 @@ class TestChangeUserData:
             assert response_new_data.json()["user"]["email"] == ChangeMethods.get_user_data(body.json()["accessToken"]).json()["user"]["email"]
             assert response_new_data.json()["user"]["name"] == ChangeMethods.get_user_data(body.json()['accessToken']).json()["user"]['name']
 
+    @allure.title('Test Change User Data Without Authorization')
+    @allure.description('Тут пытаемся менять данные пользователя без авторизации, регистрируемся на сайте, '
+                        'проверяем можно ли изменить поля сведений для пользователя'
+                        'проверяем код и тело ответа. Затем удаляем пользователя')
+    @pytest.mark.parametrize('user_data_field', ['email', 'password', 'name'])
+    # В этом методе есть баг, повторно использовать email, который уже был когда-то использован при изменении
+    # данных пользователя невозможно, даже если пользователь был удален. Необходимо каждый раз использовать
+    # уникальные почтовые адреса
+    def test_change_user_data_without_authorization(self,generate_registration_data_for_change_user_data, user_data_field):
+        # здесь мы подготавливаем тело запроса для изменения полей пользователя
+        new_user_data = GenerateBody.generate_lonely_user_data()[user_data_field]
 
+        with allure.step("Меняем данные пользователя"):
+            response_new_data = ChangeMethods.change_user_data_and_return_new_data(new_user_data, '')
 
-    def test_user_login_with_wrong_data(self, generate_registration_data_without_delete_method):
-        with allure.step("Логин под несуществующим пользователем"):
-            body = LoginMethods.login_in_system(generate_registration_data_without_delete_method['email'],
-                                                generate_registration_data_without_delete_method['password'])
         with (allure.step("Проверяем, что код ответа и тело соответствует документации")):
-            assert body.status_code == 401
-            assert body.json()["success"] == False
-            assert body.json()["message"] == MessageData.MESSEGE_INC_LOGIN_401
+            assert response_new_data.status_code == 401
+            assert response_new_data.json()["success"] == False
+            assert response_new_data.json()["message"]== "You should be authorised"
+
+
