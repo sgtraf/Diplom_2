6eea0a616ab5e1a0156c6627ca8b829a62f59f21
G Sh <conan@yandex.ru>
2025-05-05 17:36:25 +0300

    Создал первый тест создания заказа.

--- data.py
+++ data.py
@@ -5,3 +5,6 @@ class MessageData:
     MESSEGE_INC_LOGIN_401 = "email or password are incorrect"
     MESSEGE_NOT_AUTHORISED_401 = "You should be authorised"
 
+class OrderData:
+    VALID_INGREDIENT = {"ingredients": ["61c0c5a71d1f82001bdaaa6d","61c0c5a71d1f82001bdaaa6f"]}
+    INVALID_INGREDIENT = {"ingredients": ["61c0c533341d1f82001bdaaa6d---","614433271d1f82001bdaaa6f---"]}
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,9 +1,12 @@
 import allure
 import pytest
 from data import MessageData
+from data import OrderData
 from methods.login_method import LoginMethods
 from methods.change_method import ChangeMethods
 from methods.generators import GenerateBody
+from methods.order_methods import OrderMethods
+
 
 
 class TestCreateOrder:
@@ -16,16 +19,14 @@ class TestCreateOrder:
         with allure.step("Логин под пользователем"):
             body = LoginMethods.login_in_system(generate_registration_data_for_change_user_data['email'],
                                                 generate_registration_data_for_change_user_data['password'])
-        #print(body.json())
-        #здесь мы подготавливаем тело запроса для изменения полей пользователя
-        #print(new_user_data)
-        #print(generate_registration_data_for_change_user_data)
-        with allure.step("Меняем данные пользователя"):
-            response_new_data = ChangeMethods.change_user_data_and_return_new_data(new_user_data, body.json()['accessToken'])
-        #print(response_new_data.json())
-        #print(ChangeMethods.get_user_data(body.json()["accessToken"]).json())
+
+        #здесь мы подготавливаем тело запроса для заказа из валидных ингредиентов
+        new_user_data = OrderData.VALID_INGREDIENT
+        with allure.step("Делаем заказ"):
+            response_new_data = OrderMethods.take_order(new_user_data, body.json()['accessToken'])
         with (allure.step("Проверяем, что код ответа и тело соответствует документации")):
             assert response_new_data.status_code == 200
             assert response_new_data.json()["success"] == True
-            assert response_new_data.json()["user"]["email"] == ChangeMethods.get_user_data(body.json()["accessToken"]).json()["user"]["email"]
-            assert response_new_data.json()["user"]["name"] == ChangeMethods.get_user_data(body.json()['accessToken']).json()["user"]['name']
+            assert response_new_data.json()["order"]["ingredients"][0]['_id'] == OrderData.VALID_INGREDIENT['ingredients'][0]
+            assert response_new_data.json()["order"]["ingredients"][1]['_id'] == OrderData.VALID_INGREDIENT['ingredients'][1]
+
